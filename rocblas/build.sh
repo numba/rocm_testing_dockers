6b7a4dbab9255368edb7589da2d69203fbc96f63
Siu Kwan Lam <michael.lam.sk@gmail.com>
2017-03-30 11:47:59 -0500

    Update rocblas to the latest commit

--- rocblas/build.sh
+++ rocblas/build.sh
@@ -1,3 +1,3 @@
-ROCBLAS_SHA="6cc5f5fdb7cae0c8411469a666b03d24605327fd"
+ROCBLAS_SHA="e4cb0a5b6953459d8056cb7214be563c4bdbb452"
 docker build --build-arg ROCBLAS_SHA="${ROCBLAS_SHA}" -t numba_rocm_rocblas .
 
--- rocblas/getoutput.sh
+++ rocblas/getoutput.sh
@@ -2,5 +2,5 @@ mkdir -p output
 id=$(docker create numba_rocm_rocblas:latest)
 basepath=/root/rocBLAS/build/library-build/src/
 docker cp $id:$basepath/libTensile.so output/libTensile.so
-docker cp $id:$basepath/librocblas-hcc.so.0.0.2.0 output/librocblas-hcc.so
+docker cp $id:$basepath/librocblas-hcc.so.0.2.0.0 output/librocblas-hcc.so
 docker rm -v $id
